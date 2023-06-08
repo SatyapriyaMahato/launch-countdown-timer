d80cc72e6696843cb92dc1781796f4b79a6049f2
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-06-08 11:02:53 +0530

    Add flip feature

--- script.js
+++ script.js
@@ -20,18 +20,58 @@ let x = setInterval(function() {
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result "
-  document.getElementById("day_u").innerText = days;
-  document.getElementById("day_d").innerText = days;
-  document.getElementById("hour_u").innerText = hours;
-  document.getElementById("hour_d").innerText = hours;
-  document.getElementById("minute_u").innerText = minutes;
-  document.getElementById("minute_d").innerText = minutes;
-  document.getElementById("second_u").innerText = seconds;
-  document.getElementById("second_d").innerText = seconds;
-    
+
+  flip(document.querySelector("[data-day]"), days)
+  flip(document.querySelector("[data-hours]"), hours)
+  flip(document.querySelector("[data-minutes]"), minutes)
+  flip(document.querySelector("[data-seconds]"), seconds)
+
+
 //   If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("time").innerHTML = "EXPIRED";
   }
-}, 1000);
+}, 250);
+
+
+function flip(flipCard, newNumber) {
+
+  const topHalf = flipCard.querySelector(".card_u")
+  const topH1Text = topHalf.querySelector('h1')
+  const startNumber = parseInt(topH1Text.textContent)
+  if (newNumber === startNumber) return
+
+  const newNum = String(newNumber).padStart(2,'0')
+  const startNum = String(startNumber).padStart(2,'0')
+  
+  const topFlip = document.createElement("div")
+  const topFliph1 = document.createElement("h1")
+  topFlip.classList.add("top-flip")
+  const bottomFlip = document.createElement("div")
+  const bottomFliph1 = document.createElement("h1")
+  bottomFlip.classList.add("bottom-flip")
+
+  const bottomHalf = flipCard.querySelector(".card_d")
+  const bottomH1Text = bottomHalf.querySelector('h1')
+ 
+  topH1Text.textContent = startNum
+  bottomH1Text.textContent = startNum
+  topFliph1.textContent = startNum
+  bottomFliph1.textContent = newNum
+
+  topFlip.addEventListener("animationstart", e => {
+    topH1Text.textContent = newNum
+
+  })
+  topFlip.addEventListener("animationend", e => {
+    topFlip.remove()
+  })
+  bottomFlip.addEventListener("animationend", e => {
+    bottomH1Text.textContent = newNum
+    bottomFlip.remove()
+  })
+  topFlip.append(topFliph1)
+  bottomFlip.append(bottomFliph1)
+  flipCard.append(topFlip, bottomFlip)
+}
